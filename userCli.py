dabfec0f3a8c7fbfccbe60672ec49a0d667f67c9
zhajio1988 <564193687@qq.com>
2019-03-09 04:11:56 -0800

    del uselss class

--- userCli.py
+++ userCli.py
@@ -2,66 +2,11 @@ from extconfigobj import ConfigObj, ConfigObjError, Section
 import copy
 from globals import *
 
-class Option(object):
-    """Holds a configuration option and the names and locations for it.
-
-    Instantiate options using the same arguments as you would for an
-    add_arguments call in argparse. However, you have two additional kwargs
-    available:
-
-        env: the name of the environment variable to use for this option
-        ini_section: the ini file section to look this value up from
-    """
-
-    def __init__(self, *args, **kwargs):
-        self.args = args or []
-        self.kwargs = kwargs or {}
-
-    def add_argument(self, parser, **override_kwargs):
-        """Add an option to a an argparse parser."""
-        kwargs = {}
-        if self.kwargs:
-            kwargs = copy.copy(self.kwargs)
-            try:
-                del kwargs['env']
-            except KeyError:
-                pass
-            try:
-                del kwargs['ini_section']
-            except KeyError:
-                pass
-        kwargs.update(override_kwargs)
-        parser.add_argument(*self.args, **kwargs)
-
-    @property
-    def type(self):
-        """The type of the option.
-
-        Should be a callable to parse options.
-        """
-        return self.kwargs.get("type", str)
-
-    @property
-    def name(self):
-        """The name of the option as determined from the args."""
-        for arg in self.args:
-            if arg.startswith("--"):
-                return arg[2:].replace("-", "_")
-            elif arg.startswith("-"):
-                continue
-            else:
-                return arg.replace("-", "_")
-
-    @property
-    def default(self):
-        """The default for the option."""
-        return self.kwargs.get("default")
-
-
 class userCliCfg(object):
     def __init__(self, parser=None, ini_file=None):
         self.parser = parser
         self.kwargs = {}
+        self.args = []
         self.config = ConfigObj(infile=defaultCliCfgFile(), stringify=True)
         self.section = self.userCliSection
 
@@ -115,8 +60,10 @@ class userCliCfg(object):
             else:
                 self.kwargs['help'] = 'user defined option'
 
-            userCli = Option('-%s' % keyVar)
-            userCli.add_argument(self.parser, **self.kwargs)
+            #userCli = Option('-%s' % keyVar)
+            #parser.add_argument(*self.args, **kwargs)
+
+            self.parser.add_argument('-%s' % keyVar, **self.kwargs)
 
 if __name__ == '__main__':
     import argparse
