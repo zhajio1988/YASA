162e022416b4e00867b4283d30e59f59bdbfab09
jiongz <jiongz@pctbjc65s01.(none)>
2019-06-03 11:07:57 +0800

    add new user simchecker pattern

--- Simulator/userSimCheck.py
+++ Simulator/userSimCheck.py
@@ -1,8 +1,12 @@
 import re
-from simCheck import *
+from Simulator.vcsInterface import vcsSimCheck
 class userSimCheck(vcsSimCheck):
     regModelWarnPattern = r'UVM_WARNTNG .+: reporter \[UVM\/RSRC\/NOREGEX\] a resource with meta characters in the field name has been created.*'
+    cevaTestFailPattern = r'^TEST FAILED'
+    cevaReportPattern = r'SIMULATION SUMMARY'    
 
     def __init__(self):
         super (userSimCheck, self).__init__()
         self.setExcludeWarnPatterns(userSimCheck.regModelWarnPattern)
+        self.setErrPatterns(userSimCheck.cevaTestFailPattern)
+        self.setEndFlagPatterns(userSimCheck.cevaReportPattern)
